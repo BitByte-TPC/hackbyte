5ea4bb2a3af044a052ee19cb1c9df9e02c634741
Chinmay J <86140365+JawHawk@users.noreply.github.com>
2023-01-17 11:22:45 +0530

    Scene improved  and entry animation added (#14)
    scene improved and entry animation added
    
    Co-authored-by: Aksh Bansal <63552235+Aksh-Bansal-dev@users.noreply.github.com>
    Co-authored-by: Aksh Bansal <akshmark@gmail.com>

--- src/components/CanvasThree/index.jsx
+++ src/components/CanvasThree/index.jsx
@@ -1,12 +1,11 @@
-import React, { Suspense, useState } from "react";
-import { useRef } from "react";
-import { Canvas, useFrame, extend } from "@react-three/fiber";
+import React from "react";
+import { useRef ,useState} from "react";
+import { Canvas, useFrame} from "@react-three/fiber";
 import {
   OrbitControls,
   useTexture,
   Sphere,
   Sparkles,
-  shaderMaterial,
   Html,
 } from "@react-three/drei";
 import maptexture from "../../assets/textures/earth.jpg";
@@ -14,8 +13,8 @@ import disptexture from "../../assets/textures/disp2.jpg";
 import styles from "./styles.module.scss";
 import { useLocation } from "react-router-dom";
 import { AdditiveBlending, BackSide } from "three";
-import Loading from "../Loading";
 import { HeroSection } from "../HeroSection";
+import gsap from "gsap";
 
 export default function CanvasThree() {
   const canvasRef = useRef(null);
@@ -27,6 +26,8 @@ export default function CanvasThree() {
       displacementMap: disptexture,
     });
     const sph = useRef(null);
+    const [Radius1, setRadius1] = window.innerWidth <= 700 ? useState(2.1) : useState(3.5);
+    const [Radius2, setRadius2] = window.innerWidth <= 700 ? useState(1.5) : useState(2.5);
 
     const vertexShader = `varying vec3 vertexNormal;
     void main() {vertexNormal = normalize(normalMatrix * normal);
@@ -36,14 +37,23 @@ export default function CanvasThree() {
     void main(){float intensity = pow(0.5 - dot(vertexNormal,vec3(0,0,1.0)),2.4);
     gl_FragColor = vec4(0.1,0.0,1.0,0.75)* intensity;}`;
 
-    useFrame((e) => {
+    window.addEventListener('resize',()=>{
+      if (window.innerWidth <= 700) {
+        setRadius1(2.1);
+        setRadius2(1.5)
+      } else {
+        setRadius1(3.5);
+        setRadius2(2.5);
+      }
+    })  
+    useFrame(() => {
       sph.current.rotation.y += 0.001;
       sph.current.rotation.x += 0.002;
     });
 
     return (
       <group ref={sph}>
-        <Sphere args={[3.5, 100, 100]}>
+        <Sphere args={[Radius1, 100, 100]}>
           <shaderMaterial
             vertexShader={vertexShader}
             side={BackSide}
@@ -51,7 +61,7 @@ export default function CanvasThree() {
             blending={AdditiveBlending}
           />
         </Sphere>
-        <Sphere args={[2.5, 100, 100]}>
+        <Sphere args={[Radius2, 100, 100]}>
           <meshStandardMaterial
             transparent
             opacity={0.85}
@@ -69,7 +79,8 @@ export default function CanvasThree() {
     <Canvas
       className={styles.canvas}
       ref={canvasRef}
-      camera={{ position: [0, 0, 7] }}
+      camera={{ position: [15, 15, 15] }}
+      onCreated={(state)=>{gsap.to(state.camera.position,{duration:2.5,delay:0,z:7,x:0,y:0, ease:'Power3.easeOut'})}}
     >
       <Sparkles count={200} scale={[30, 30, 30]} size={2} speed={2.5} />
       <directionalLight position={[2, -5, 7]} intensity={1} />
@@ -77,22 +88,14 @@ export default function CanvasThree() {
 
       {location.pathname == "/" && (
         <>
-          <Suspense
-            fallback={
-              <Html>
-                <Loading />
-              </Html>
-            }
-          >
-            <SphereMain />
-            <Html>
-              <HeroSection />
-            </Html>
-          </Suspense>
+          <SphereMain />
+          <Html>
+            <HeroSection />
+          </Html>
         </>
       )}
 
-      <OrbitControls dampingFactor={0.15} enableZoom={false} />
+      <OrbitControls dampingFactor={0.15} enableZoom={false} enablePan={false}/>
     </Canvas>
   );
 }
