a09df31de76e91050283038f570b79c0cab95209
Prajjwal kapoor <prajjwal.kapoor.mail@gmail.com>
2023-01-18 04:20:16 +0530

    test: check loading

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Loading from "./components/Loading";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
-import { lazy } from "react";
+// import { lazy } from "react";
 
-const App = lazy(() => import("./App"));
+// const App = lazy(() => import("./App"));
+import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Suspense fallback={<Loading />}>
