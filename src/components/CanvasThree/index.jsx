a04bbfca01630d974957e8035c58fc7c4b1d9329
jawhawk <mavenal2143@gmail.com>
2023-01-26 01:22:14 +0530

    Update: Scene Improved

--- src/components/CanvasThree/index.jsx
+++ src/components/CanvasThree/index.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   OrbitControls,
   useTexture,
   Sphere,
   Sparkles,
   Html,
-  Cloud
+  Cloud,
 } from "@react-three/drei";
+import cloud from '../../assets/cloud.png'
 import maptexture from "../../assets/textures/earth.jpg";
 import disptexture from "../../assets/textures/disp2.jpg";
 import styles from "./styles.module.scss";
 import { useLocation } from "react-router-dom";
-import { AdditiveBlending, BackSide, Clock } from "three";
+import { AdditiveBlending, BackSide, Vector3 } from "three";
 import { HeroSection } from "../HeroSection";
-import gsap from "gsap";
 
 export default function CanvasThree() {
   const canvasRef = useRef(null);
   const location = useLocation(); // use this to show earth only on homepage
   const [cloudOpacity, setcloudOpacity] = useState(0)
+  
   function SphereMain() {
-    const state = useThree();
     const colorTexture = useTexture({
       map: maptexture,
       displacementMap: disptexture,
     });
     const sph = useRef(null);
     const [Radius1, setRadius1] =
-      window.innerWidth <= 700 ? useState(3.2) : useState(4.3);
+      window.innerWidth <= 700 ? useState(3.2) : useState(4);
     const [Radius2, setRadius2] =
-      window.innerWidth <= 700 ? useState(2.2) : useState(2.8);
+      window.innerWidth <= 700 ? useState(2.2) : useState(2.5);
 
     const vertexShader = `varying vec3 vertexNormal;
     void main() {vertexNormal = normalize(normalMatrix * normal);
@@ -40,32 +40,21 @@ export default function CanvasThree() {
     const fragmentShader = `varying vec3 vertexNormal;
     void main(){float intensity = pow(0.5 - dot(vertexNormal,vec3(0,0,1.0)),2.4);
     gl_FragColor = vec4(0.1,0.0,1.0,0.65)* intensity;}`;
-
-    useEffect(() => {
-      state.camera.position.set(15, 15, 15);
-      gsap.to(state.camera.position, {
-        duration: 2.5,
-        delay: 0,
-        z: 7,
-        x: 0,
-        y: 0,
-        ease: "Power3.easeOut",
-      });
-    }, []);
-
+    
     window.addEventListener("resize", () => {
       if (window.innerWidth <= 700) {
         setRadius1(3.2);
         setRadius2(2.2);
       } else {
-        setRadius1(4.3);
-        setRadius2(2.8);
+        setRadius1(4);
+        setRadius2(2.5);
       }
     });
 
     useFrame((state) => {
       sph.current.rotation.y += 0.003;
       sph.current.position.z = 0.6*Math.sin(state.clock.elapsedTime)
+      sph.current.position.lerp(new Vector3(state.mouse.x*0.5,state.mouse.y*0.3,7),0.05)     
     });
 
     return (
@@ -99,15 +88,17 @@ export default function CanvasThree() {
     <Canvas
       className={styles.canvas}
       ref={canvasRef}
-      camera={{ position: [15, 15, 15] }}
+      camera={{ position: [0,0,7] }}
+      // style={{background:'linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(22,20,46,1) 50%, rgba(0,0,0,1) 100%'}}
     >
-      <Sparkles count={200} scale={[30, 30, 30]} size={2} speed={2.5} />
+      <Sparkles count={250} scale={[30, 30, 30]} size={2} speed={2.5} />
       <directionalLight position={[2, -5, 7]} intensity={1} />
       <ambientLight intensity={1.5} />
-      <Cloud speed={1} opacity={cloudOpacity} color="#2160a3" />
+      <Cloud speed={1} opacity={cloudOpacity} texture={cloud} color="#2160a3" />
       {location.pathname == "/" && (
         <>
           <SphereMain />
+          
           {/* <Html>
             <HeroSection />
           </Html> */}
@@ -118,6 +109,7 @@ export default function CanvasThree() {
         dampingFactor={0.15}
         enableZoom={false}
         enablePan={false}
+        enableRotate={false}
       />
     </Canvas>
   );
