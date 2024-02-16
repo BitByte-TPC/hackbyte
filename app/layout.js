eeed298b18f6936ed5b630e940536df72e4ffd01
Vansh Mittal <mittalvansh69@gmail.com>
2024-02-17 01:31:05 +0530

    add google-analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import "react-toastify/dist/ReactToastify.css";
 import "./clash_grotesk.css";
 
@@ -24,6 +25,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-50RJG4PPLT"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-50RJG4PPLT');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
