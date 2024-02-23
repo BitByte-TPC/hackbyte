6a2cf99fb0b4b4dfd7e9b0afaef00cca8efbb868
Vansh Mittal <mittalvansh69@gmail.com>
2024-02-23 16:37:58 +0530

    minor layout fix

--- app/schedule/page.jsx
+++ app/schedule/page.jsx
@@ -8,8 +8,7 @@ export const metadata = {
 export default function Schedule() {
   return (
     <div
-      className="flex flex-col min-h-screen md:min-h-fit
-        bg-cover p-4 md:px-12 md:pt-8"
+      className="flex flex-col min-h-screen bg-cover p-4 md:px-12 md:pt-8"
       style={{
         backgroundColor: "#000",
         backgroundImage: "url(/background.png)",
