fceb2b036a2c0bd7851582db9e382d0f87e17040
Vansh Mittal <mittalvansh69@gmail.com>
2024-03-05 00:26:02 +0530

    add seo for partners page

--- app/partners/page.jsx
+++ app/partners/page.jsx
@@ -14,6 +14,25 @@ import balsamiq from "@/public/partnersPage/balsamiq.webp";
 import CircleAnimation from "@/components/CircleAnimation";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "Partners | HackByte",
+  description:
+    "Explore the invaluable support from our esteemed sponsors and partners at HackByte – the premier hackathon hosted by IIITDMJ. Discover their contributions and partnerships, playing a pivotal role in empowering the coding community.",
+  keywords:
+    "Sponsors, Partners, Hackathon, IIITDMJ, Hackbyte, Coding, Programming, Tech, Collaboration, Innovation, Community Support, Technology Events",
+  openGraph: {
+    title: "Partners | HackByte",
+    description:
+      "Explore the invaluable support from our esteemed sponsors and partners at HackByte – the premier hackathon hosted by IIITDMJ. Discover their contributions and partnerships, playing a pivotal role in empowering the coding community.",
+    url: "https://hackbyte.in/partners",
+    images:
+      "https://res.cloudinary.com/drtmfrghg/image/upload/v1708016443/opengraph-image_vkiopn.jpg",
+    siteName: "HackByte - IIITDMJ Hackathon",
+    type: "website",
+    locale: "en_US",
+  },
+};
+
 const NormalSponsors = () => {
   return (
     <>
@@ -58,7 +77,7 @@ const InKindSponsors = () => {
       />
       <SponsorCard
         sponsor={"Balsamiq"}
-        category={"community Sponsor"}
+        category={"Community Sponsor"}
         sponsorimgsrc={balsamiq}
       />
       <SponsorCard
@@ -78,32 +97,35 @@ const InKindSponsors = () => {
 const Partners = () => {
   return (
     <>
-      <div className="bg-[#101010] flex flex-col items-center min-h-screen lg:px-20 px-4 py-52">
-          <Tabs
-            defaultValue="Sponsors"
-            className="flex flex-col justify-center items-center text-white"
-          >
-            <TabsList className="mb-16 w-[19rem] min-w-[321px]:w-80 sm:w-[488px]">
-              <TabsTrigger
-                value="Sponsors"
-                className="sm:p-4 p-2 sm:w-60 w-40  min-[300px]:text-lg sm:text-2xl text-xl leading-7"
-              >
-                Sponsors
-              </TabsTrigger>
-              <TabsTrigger
-                value="InKind Sponsors"
-                className="sm:p-4 p-2 sm:w-60 w-40  min-[300px]:text-lg sm:text-2xl text-xl leading-7"
-              >
-                In Kind Sponsors
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="Sponsors">
-              <NormalSponsors />
-            </TabsContent>
-            <TabsContent value="InKind Sponsors">
-              <InKindSponsors />
-            </TabsContent>
-          </Tabs>
+      <div
+        className="bg-[#101010] flex flex-col items-center min-h-screen 
+          lg:px-20 px-4 py-52"
+      >
+        <Tabs
+          defaultValue="Sponsors"
+          className="flex flex-col justify-center items-center text-white"
+        >
+          <TabsList className="mb-16 w-[19rem] min-w-[321px]:w-80 sm:w-[488px]">
+            <TabsTrigger
+              value="Sponsors"
+              className="sm:p-4 p-2 sm:w-60 w-40 min-[300px]:text-lg sm:text-2xl text-xl leading-7"
+            >
+              Sponsors
+            </TabsTrigger>
+            <TabsTrigger
+              value="InKind Sponsors"
+              className="sm:p-4 p-2 sm:w-60 w-40 min-[300px]:text-lg sm:text-2xl text-xl leading-7"
+            >
+              In Kind Sponsors
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="Sponsors">
+            <NormalSponsors />
+          </TabsContent>
+          <TabsContent value="InKind Sponsors">
+            <InKindSponsors />
+          </TabsContent>
+        </Tabs>
       </div>
 
       <div className="bg-black w-full h-full py-24 lg:py-40">
@@ -113,8 +135,8 @@ const Partners = () => {
         >
           <p
             className="max-w-3xl text-6xl md:text-[5.5rem] lg:text-[7rem] xl:text-[9rem] 
-                    text-white font-normal font-['Clash Grotesk'] md:leading-[8rem] 
-                        lg:leading-[10rem] tracking-tighter md:tracking-[-0.2rem]"
+            text-white font-normal font-['Clash Grotesk'] md:leading-[8rem] 
+              lg:leading-[10rem] tracking-tighter md:tracking-[-0.2rem]"
           >
             Empower
             <br /> Your Digital
