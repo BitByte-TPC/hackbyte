29a8a2c3ca57b2ef14e551799877313bafaff750
Pratik Solanki <128511266+Pratik2026@users.noreply.github.com>
2024-03-14 03:50:05 +0530

    Update Sponsors and also added some animations. (#66)

--- components/ui/cards.jsx
+++ components/ui/cards.jsx
@@ -1,28 +1,51 @@
+"use client";
+import React from "react";
 import Image from "next/image";
-import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+import { motion } from "framer-motion";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
-const SponsorCard = ({ sponsor, category, sponsorimgsrc, site }) => {
+const SponsorCard = ({ index, sponsor, category, sponsorimgsrc, site }) => {
   return (
     <>
-      <a href={site} target="_blank" rel="noreferrer">
-        <div className="w-[295px] min-w-[306px] xl:w-[350px] bg-[#1B1B1B] hover:bg-[#353535] rounded-lg shadow text-white">
-          <Image
-            className="rounded-t-lg w-[306px] xl:w-[350px] outline-none"
-            src={sponsorimgsrc}
-            placeholder="blur"
-            alt=""
-          />
-          <div className="flex justify-between items-start p-6">
-            <div className="flex flex-col gap-2">
-              <p className="sm:text-2xl text-xl">{sponsor}</p>
-              <p className="text-[#C3C3C3] sm:text-2xl text-xl  leading-9 font-[Inter]">
-                {category}
-              </p>
+      <motion.div
+        className="card"
+        initial={{
+          opacity: 0,
+          x: index % 3 === 0 ? -50 : index % 3 === 1 ? 0 : 50,
+          y: index % 3 === 1 ? 20 : 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+          y: 0,
+          transition: {
+            duration: 1,
+          },
+        }}
+        viewport={{ once: true }}
+      >
+        <a href={site} target="_blank" rel="noreferrer">
+          <div className="w-[295px] min-w-[306px] xl:w-[350px] bg-[#1B1B1B] hover:bg-[#353535] rounded-lg shadow text-white">
+            <Image
+              className="rounded-t-lg w-[306px] h-[181px] xl:w-[350px] xl:h-[207px] outline-none"
+              src={sponsorimgsrc}
+              placeholder="blur"
+              alt=""
+            />
+            <div className="flex justify-between items-start p-6">
+              <div className="flex flex-col gap-2">
+                <p className="sm:text-2xl text-xl">{sponsor}</p>
+                <p className="text-[#C3C3C3] sm:text-2xl text-xl  leading-9 font-[Inter]">
+                  {category}
+                </p>
+              </div>
+              <div className="hover:bg-[#1B1B1B] mt-1 p-1">
+                <ArrowRightIcon className="w-6 h-6 p-[2px] hover:-rotate-45 transition-transform duration-300 ease-in-out" />
+              </div>
             </div>
-            <ArrowTopRightIcon className="w-6 h-6 pt-2" />
           </div>
-        </div>
-      </a>
+        </a>
+      </motion.div>
     </>
   );
 };
