e64ccfb38300c9a505e65261fcb0934adfdb0cdc
rohansen856 <rohansen856@gmail.com>
2024-12-25 02:13:21 +0530

    new: added faq section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,32 @@ module.exports = {
   			supporting: mytheme.colors.supporting
   		},
   		fontFamily: {
-  			'gotham': ['var(--font-gotham)']
+  			gotham: [
+  				'var(--font-gotham)'
+  			]
   		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
   	}
   },
   plugins: [import("tailwindcss-animate")],
