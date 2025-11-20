fbe36bff3e516e998343f60ae2be4f2595a29e4c
owlpharoah <sreeharirathish128@gmail.com>
2025-11-20 13:50:25 +0530

    allowing redirects

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/aftermovie",
+        destination: "https://www.youtube.com/watch?v=xKLlWAJvdDQ",
+        permanent: false,
+      },
+      {
+        source: "/prospectus",
+        destination: "https://drive.google.com/file/d/1I5ZFxc1wKfJIN8JRd-9YV2HmfQkOblrr/view",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
